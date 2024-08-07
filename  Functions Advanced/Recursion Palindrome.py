fb54bb76cd2fecd43461e843f4c4ef86decb4515
Georgi Raykov <graykov7@gmail.com>
2024-08-07 13:33:02 +0300

    Lab: Functions Advanced

---
+++  Functions Advanced/Recursion Palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(word, index):
+
+    if index >= len(word) // 2:
+
+        return f"{word} is a palindrome"
+
+    first = word[index]
+    last = word[-1 - index]
+
+    if first != last:
+        return f"{word} is not a palindrome"
+
+    return palindrome(word, index + 1)
+
+
+print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
